6693e6821b69d29b1b165046c973993eac8a5b83
oceana1129 <oceana1129@gmail.com>
2026-07-16 21:43:27 -0700

    fixed server error

--- backend/src/server.js
+++ backend/src/server.js
@@ -21,16 +21,16 @@ dotenv.config();
 const PORT = process.env.PORT || 5001;
 // set up express
 const app = express();
-// can read json sent from client
-app.use(express.json());
-// set up rate limiter
-app.use(rateLimiter);
 // use use middleware cors on front end url
 app.use(
   cors({
     origin: "http://localhost:5173",
   }),
 );
+// can read json sent from client
+app.use(express.json());
+// set up rate limiter
+// app.use(rateLimiter);
 
 // set up routes
 app.use("/api/test", testRoutes);
